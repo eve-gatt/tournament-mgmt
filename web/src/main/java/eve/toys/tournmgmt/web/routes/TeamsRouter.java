f5be3c63c9e42777fa0e300bfc9ddc54cf248323
Gatt <eve.gatt2111@gmail.com>
2020-08-22 23:30:10 +0100

    pages for importing teams as tsv

--- web/src/main/java/eve/toys/tournmgmt/web/routes/TeamsRouter.java
+++ web/src/main/java/eve/toys/tournmgmt/web/routes/TeamsRouter.java
@@ -6,6 +6,10 @@ import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.api.RequestParameters;
+import io.vertx.ext.web.api.validation.HTTPRequestValidationHandler;
+import io.vertx.ext.web.api.validation.ParameterTypeValidator;
+import io.vertx.ext.web.handler.BodyHandler;
 import toys.eve.tournmgmt.common.util.RenderHelper;
 import toys.eve.tournmgmt.db.DbClient;
 
@@ -23,10 +27,33 @@ public class TeamsRouter {
         router.get("/:tournamentUuid/teams").handler(this::manage);
         router.get("/:tournamentUuid/teams/data").handler(this::teamsData);
         router.get("/:tournamentUuid/teams/import").handler(this::importTeams);
+        HTTPRequestValidationHandler importValidator = HTTPRequestValidationHandler.create()
+                .addFormParamWithCustomTypeValidator("tsv",
+                        ParameterTypeValidator.createStringTypeValidator("\\p{ASCII}+", 3, null, null),
+                        true,
+                        false);
+
+        router.post("/:tournamentUuid/teams/import")
+                .handler(BodyHandler.create())
+                .handler(importValidator)
+                .handler(this::handleImport)
+                .failureHandler(this::handleImportFail);
     }
 
     public static Router routes(Vertx vertx, RenderHelper render) {
         return new TeamsRouter(vertx, render).router();
+    }
+
+    private void handleImportFail(RoutingContext ctx) {
+        RenderHelper.doRedirect(ctx.response(),
+                "/auth/tournament/" + ctx.request().getParam("tournamentUuid") + "/teams/import");
+    }
+
+    private void handleImport(RoutingContext ctx) {
+        RequestParameters params = ctx.get("parsedParameters");
+        System.out.println(params.formParameter("tsv"));
+        RenderHelper.doRedirect(ctx.response(),
+                "/auth/tournament/" + ctx.request().getParam("tournamentUuid") + "/teams/import");
     }
 
     private void teamsData(RoutingContext ctx) {
@@ -53,7 +80,14 @@ public class TeamsRouter {
     }
 
     private void importTeams(RoutingContext ctx) {
-        render.renderPage(ctx, "/teams/import", new JsonObject());
+        render.renderPage(ctx,
+                "/teams/import",
+                new JsonObject().put("placeholder",
+                        "Paste values straight from spreadsheet as two column, name and captain, e.g. \n" +
+                                "The Tuskers\tMira Chieve\n" +
+                                "Big Alliancia\tCaptain Jack\n" +
+                                "\n" +
+                                "This is the default format when copy and pasting a range from a spreadsheet."));
     }
 
     private Router router() {
