2f116a164b80a753f732c4dd79dc6e2524e64dd7
Gatt <eve.gatt2111@gmail.com>
2020-08-23 00:50:12 +0100

    example of validating team import and alliance membership of captains

--- web/src/main/java/eve/toys/tournmgmt/web/routes/TeamsRouter.java
+++ web/src/main/java/eve/toys/tournmgmt/web/routes/TeamsRouter.java
@@ -1,9 +1,13 @@
 package eve.toys.tournmgmt.web.routes;
 
 import eve.toys.tournmgmt.web.Branding;
+import io.vertx.core.CompositeFuture;
+import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.EventBus;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import io.vertx.ext.auth.oauth2.AccessToken;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.api.RequestParameters;
@@ -13,8 +17,15 @@ import io.vertx.ext.web.handler.BodyHandler;
 import toys.eve.tournmgmt.common.util.RenderHelper;
 import toys.eve.tournmgmt.db.DbClient;
 
+import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class TeamsRouter {
 
+    public static final String ESI_BASE = "https://esi.evetech.net/latest";
     private final EventBus eventBus;
     private final RenderHelper render;
     private Router router;
@@ -51,9 +62,90 @@ public class TeamsRouter {
 
     private void handleImport(RoutingContext ctx) {
         RequestParameters params = ctx.get("parsedParameters");
-        System.out.println(params.formParameter("tsv"));
+        String tsv = params.formParameter("tsv").getString();
+        AccessToken token = (AccessToken) ctx.user();
+
+        String[] rows = tsv.split("[\\r\\n]+");
+        List<Future> searches = Arrays.stream(rows)
+                .flatMap(row -> {
+                    String[] cols = row.split("\t");
+                    return Stream.of(
+                            checkMembership(
+                                    token,
+                                    checkAlliance(token, cols[0]),
+                                    checkCharacter(token, cols[1])));
+                })
+                .collect(Collectors.toList());
+
+        CompositeFuture.all(searches)
+                .onSuccess(f -> {
+                    System.out.println(new JsonArray(f.list()).encodePrettily());
+                }).onFailure(Throwable::printStackTrace);
+
         RenderHelper.doRedirect(ctx.response(),
                 "/auth/tournament/" + ctx.request().getParam("tournamentUuid") + "/teams/import");
+    }
+
+    private Future<JsonObject> checkMembership(AccessToken token,
+                                               Future<JsonObject> checkAlliance,
+                                               Future<JsonObject> checkCharacter) {
+        return Future.future(promise ->
+                CompositeFuture.all(checkAlliance, checkCharacter)
+                        .onSuccess(f -> {
+                            JsonObject alliance = f.resultAt(0);
+                            JsonObject character = f.resultAt(1);
+                            JsonObject out = new JsonObject();
+                            out.put("alliance", alliance)
+                                    .put("character", character);
+                            if (alliance.getJsonArray("result") != null
+                                    && character.getJsonArray("result") != null) {
+                                token.fetch(ESI_BASE + "/characters/" + character.getJsonArray("result").getInteger(0),
+                                        ar -> {
+                                            if (ar.failed()) {
+                                                promise.fail(ar.cause());
+                                                return;
+                                            }
+                                            Integer allianceId = ar.result().body().toJsonObject()
+                                                    .getInteger("alliance_id");
+                                            out.put("membership", allianceId);
+                                            promise.complete(out);
+                                        });
+                            } else {
+                                promise.complete(out);
+                            }
+                        })
+                        .onFailure(Throwable::printStackTrace)
+        );
+    }
+
+    private Future<JsonObject> checkAlliance(AccessToken token, String alliance) {
+        return Future.future(promise ->
+                token.fetch(ESI_BASE + "/search/?categories=alliance&strict=true&search="
+                                + URLEncoder.encode(alliance),
+                        ar -> {
+                            if (ar.failed()) {
+                                promise.fail(ar.cause());
+                                return;
+                            }
+                            promise.complete(new JsonObject()
+                                    .put("alliance", alliance)
+                                    .put("result", ar.result().body().toJsonObject().getJsonArray("alliance")));
+                        }));
+    }
+
+    private Future<JsonObject> checkCharacter(AccessToken token, String character) {
+        return Future.future(promise ->
+                token.fetch(ESI_BASE + "/search/?categories=character&strict=true&search="
+                                + URLEncoder.encode(character),
+                        ar -> {
+                            if (ar.failed()) {
+                                promise.fail(ar.cause());
+                                return;
+                            }
+                            promise.complete(new JsonObject()
+                                    .put("character", character)
+                                    .put("result", ar.result().body().toJsonObject().getJsonArray("character")));
+                        }));
     }
 
     private void teamsData(RoutingContext ctx) {
