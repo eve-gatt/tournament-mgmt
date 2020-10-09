5529728c5e2d80d9e872b3246e26135223ad3f1c
Gatt <eve-gatt2111@gmail.com>
2020-10-09 13:56:42 +0100

    add counts

--- web/src/main/resources/web-js/teams/manage.js
+++ web/src/main/resources/web-js/teams/manage.js
@@ -1,5 +1,9 @@
 (function () {
 
+    let isRed = d => d.member_count === 0;
+    let isYellow = d => (d.member_count > 0 && !d.locked) || (d.locked && d.message);
+    let isGreen = d => d.locked && !d.message;
+
     function actions(sel) {
         sel.append('a')
             .attr('href', d => '/auth/tournament/' + uuid + '/teams/' + d.uuid + '/edit')
@@ -11,13 +15,18 @@
 
     function renderTeams(data) {
         if (data.length > 0) {
+
+            d3.select('.red .count').text(data.filter(isRed).length);
+            d3.select('.yellow .count').text(data.filter(isYellow).length);
+            d3.select('.green .count').text(data.filter(isGreen).length);
+
             d3.select('.teams .no-teams').style('display', 'none');
             data.sort((a, b) => d3.ascending(a.name, b.name));
             var teams = d3.select('.teams table tbody').selectAll('tr').data(data);
             var entering = teams.enter().append('tr');
-            entering.classed('red', d => d.member_count === 0);
-            entering.classed('yellow', d => (d.member_count > 0 && !d.locked) || (d.locked && d.message));
-            entering.classed('green', d => d.locked && !d.message);
+            entering.classed('red', isRed);
+            entering.classed('yellow', isYellow);
+            entering.classed('green', isGreen);
             var teamCell = entering.append('td').append('div').classed('team-name', true);
             teamCell.append('img').attr('src', d => d.logo + "?size=64");
             teamCell.append('span').text(d => d.name.length > 22 ? d.name.substring(0, 20) + '...' : d.name);
