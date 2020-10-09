0558875b7c5a133902bd5db61430b9bf0eca03e7
Gatt <eve-gatt2111@gmail.com>
2020-10-09 13:45:41 +0100

    fix color-coding logic

--- web/src/main/resources/web-js/teams/manage.js
+++ web/src/main/resources/web-js/teams/manage.js
@@ -16,8 +16,8 @@
             var teams = d3.select('.teams table tbody').selectAll('tr').data(data);
             var entering = teams.enter().append('tr');
             entering.classed('red', d => d.member_count === 0);
-            entering.classed('yellow', d => d.member_count > 0 && !d.locked);
-            entering.classed('green', d => d.locked);
+            entering.classed('yellow', d => (d.member_count > 0 && !d.locked) || (d.locked && d.message));
+            entering.classed('green', d => d.locked && !d.message);
             var teamCell = entering.append('td').append('div').classed('team-name', true);
             teamCell.append('img').attr('src', d => d.logo + "?size=64");
             teamCell.append('span').text(d => d.name.length > 22 ? d.name.substring(0, 20) + '...' : d.name);
